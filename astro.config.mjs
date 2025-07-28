5da394780fd145e259accf3b206b10c47bb789fa
*EIDG6TQk4d <todoroki.kyohei@tenpos.com>
2025-07-28 18:00:50 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import partytown from '@astrojs/partytown';
+// import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  integrations: []
 });
